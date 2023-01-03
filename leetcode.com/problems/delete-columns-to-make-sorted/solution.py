fcb0b97e0f702e6c4da07a9e0355688dd7c0efd3
Faizan Khalid <iamfaizankhalid@gmail.com>
2023-01-04 00:04:13 +0500

    Delete Columns to Make Sorted

--- leetcode.com/problems/delete-columns-to-make-sorted/solution.py
+++ leetcode.com/problems/delete-columns-to-make-sorted/solution.py
@@ -1,21 +1,15 @@
 class Solution:
-    def minDeletionSize(self, strs: List[str]) -> int:
-        l = len(strs[0])
+	def minDeletionSize(self, strs: List[str]) -> int:
+		l = len(strs[0])
 
-        toRemoveCount = 0
+		toRemoveCount = 0
 
-        for c in range(l):
-            sorted=True
-            x = strs[0][c]
-            for i in range(len(strs)):
-                if strs[i][c]<x:
-                    return False
-                x = strs[i][c]
-            if not sorted:
-                toRemoveCount+=1
+		for c in range(l):
+			x = strs[0][c]
+			for i in range(len(strs)):
+				if strs[i][c] < x:
+					toRemoveCount += 1
+					break
+				x = strs[i][c]
 
-        return toRemoveCount
-
-    
-
-            
+		return toRemoveCount
