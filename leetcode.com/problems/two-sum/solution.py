5b276b900e79088d6e134adc0e547c9bff159a33
Faizan Khalid <iamfaizankhalid@gmail.com>
2023-01-28 18:56:51 +0500

    1. Two Sum

---
+++ leetcode.com/problems/two-sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		indexes = {}
+		for i, num in enumerate(nums):
+			if target - num in indexes:
+				return [i, indexes[target - num]]
+
+			indexes[num] = i
