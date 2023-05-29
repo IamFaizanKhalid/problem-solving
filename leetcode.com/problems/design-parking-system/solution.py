3116f229003bf1a9d66e6f6ced0039aec62f021a
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-05-29 19:52:14 +0500

    Used array

--- leetcode.com/problems/design-parking-system/solution.py
+++ leetcode.com/problems/design-parking-system/solution.py
@@ -1,28 +1,12 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
+        self.slots = [0,big,medium,small]
         
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            self.big -= 1
-            return self.big >= 0
-            
-        if carType == 2:
-            self.medium -= 1
-            return self.medium >= 0
-            
-        if carType == 3:
-            self.small -= 1
-            return self.small >= 0
-
-        return false
-
-            
-        
+        self.slots[carType] -= 1
+        return self.slots[carType] >= 0
 
 
 # Your ParkingSystem object will be instantiated and called as such:
